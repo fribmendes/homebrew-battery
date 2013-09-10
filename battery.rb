23cd93fed9a3e68a502418a8f6796d7eadbec220
Nicolas Goles <goles.nicolas@gmail.com>
2013-09-10 18:01:35 -0300

    Updated to battery 1.2
    Battery 1.2 has it's own stable release now so that the SHA1 won't break
    again.
    
    _NG

--- battery.rb
+++ battery.rb
@@ -3,8 +3,8 @@ require 'formula'
 class Battery < Formula
   homepage 'https://github.com/Goles/Battery'
   url 'https://raw.github.com/Goles/Battery/master/battery'
-  sha1 '567446640f9435ce22c3efe5927cb5753080a797'
-  version '1.1'
+  sha1 'f9d2fa8103346d3eda7174a3939ced910d323480'
+  version '1.2'
 
   depends_on 'spark'
 
