ee3e44383b80512802413d0357f8e350ef27d0c1
Nicolas Goles <goles.nicolas@gmail.com>
2013-09-10 19:30:12 -0300

    Fixed the SHA1 hash

--- battery.rb
+++ battery.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Battery < Formula
   homepage 'https://github.com/Goles/Battery'
   url 'https://github.com/Goles/Battery/archive/v1.2.tar.gz'
-  sha1 'f9d2fa8103346d3eda7174a3939ced910d323480'
+  sha1 '1bf3ccc7ec3bf8ada63c0d8aa81e9cdad569f906'
   version '1.2'
 
   depends_on 'spark'
