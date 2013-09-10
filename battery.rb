4a3dcd9aad594bf3181e50a253713108d7f12b16
Nicolas Goles <goles.nicolas@gmail.com>
2013-09-10 19:26:16 -0300

    Fixed the url for the tap

--- battery.rb
+++ battery.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class Battery < Formula
   homepage 'https://github.com/Goles/Battery'
-  url 'https://raw.github.com/Goles/Battery/master/battery'
+  url 'https://github.com/Goles/Battery/archive/v1.2.tar.gz'
   sha1 'f9d2fa8103346d3eda7174a3939ced910d323480'
   version '1.2'
 
