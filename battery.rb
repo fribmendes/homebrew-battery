26ba8b4d0bc3000a21c029f0ade66c7853108384
Fernando Mendes <devfrmendes@gmail.com>
2017-03-20 12:00:05 +0000

    Allow HEAD install

--- battery.rb
+++ battery.rb
@@ -5,6 +5,7 @@ class Battery < Formula
   url 'https://github.com/Goles/Battery/archive/1.4.tar.gz'
   sha256 '99d95baa9532108fed518e4b01500a764ad59cb31611ce10c28138f28ebc3037'
   version '1.4'
+  head 'https://github.com/Goles/Battery.git'
 
   depends_on 'spark'
 
