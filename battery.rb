db967e77fb20c2ee3bb78e2d0d99e68950545e5a
Nicolas Goles <nicolas.goles@microsoft.com>
2017-12-18 11:04:14 -0800

    Updated tap version to 1.4

--- battery.rb
+++ battery.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class Battery < Formula
   homepage 'https://github.com/Goles/Battery'
-  url 'https://github.com/Goles/Battery/archive/1.3.tar.gz'
-  sha256 'b724ff05fdcbb7e37e88432f9ae0f5235d5b22b1fd7c30d6973d91d27ac7a495'
-  version '1.3'
+  url 'https://github.com/Goles/Battery/archive/1.4.tar.gz'
+  sha256 '99d95baa9532108fed518e4b01500a764ad59cb31611ce10c28138f28ebc3037'
+  version '1.4'
 
   depends_on 'spark'
 
